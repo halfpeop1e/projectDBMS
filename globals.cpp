319719cd669ffc315899c384092cb05de9217ced
AAcwin <153008458+AAcwin@users.noreply.github.com>
2025-05-14 20:01:40 +0800

    完成ALTER...ADD...功能

--- globals.cpp
+++ globals.cpp
@@ -4,7 +4,7 @@ QString currentUser="guest";
 Auth::Role currentRole;
 QTextBrowser *showinShell = nullptr;
 QString usingDatabase;
-QString dbRoot = "/Users/fengzhu/Resource";             // 所有数据库存放路径
-QString userFile = "/Users/fengzhu/Resource/users.txt"; // 用户信息存储文件
-QString logFile = "/Users/fengzhu/Resource/log.txt";    // 日志文件
+QString dbRoot = "E:/dbms/Resource";             // 所有数据库存放路径
+QString userFile = "E:/dbms/Resource/users.txt"; // 用户信息存储文件
+QString logFile = "E:/dbms/Resource/log.txt";    // 日志文件
 MainWindow *mainWindow = nullptr;
